5ad4da7f286761f8cac9306271fed3bf71f2c4ab
Mike Rothfuss <git@mikerothfuss.com>
2025-01-25 03:39:55 -0700

    legacy agesa: fixed errors in the f15 codebase

--- src/drivers/amd/agesa/def_callouts.c
+++ src/drivers/amd/agesa/def_callouts.c
@@ -115,27 +115,20 @@ AGESA_STATUS agesa_Reset (UINT32 Func, UINTN Data, VOID *ConfigPtr)
 
 AGESA_STATUS agesa_RunFuncOnAp (UINT32 Func, UINTN Data, VOID *ConfigPtr)
 {
-	AMD_CONFIG_PARAMS *StdHeader = ConfigPtr;
 	AGESA_STATUS status;
+
+#if HAS_LEGACY_WRAPPER
+	status = AmdLateRunApTask((AP_EXE_PARAMS*)ConfigPtr);
+#else
+	AMD_CONFIG_PARAMS *StdHeader = ConfigPtr;
 	AP_EXE_PARAMS ApExeParams;
 
 	memset(&ApExeParams, 0, sizeof(AP_EXE_PARAMS));
 
-	if (HAS_LEGACY_WRAPPER) {
-		ApExeParams.StdHeader.AltImageBasePtr = 0;
-		ApExeParams.StdHeader.CalloutPtr = &GetBiosCallout;
-		ApExeParams.StdHeader.Func = 0;
-		ApExeParams.StdHeader.ImageBasePtr = 0;
-	} else {
-		memcpy(&ApExeParams.StdHeader, StdHeader, sizeof(*StdHeader));
-	}
+	memcpy(&ApExeParams.StdHeader, StdHeader, sizeof(*StdHeader));
 
 	ApExeParams.FunctionNumber = Func;
 	ApExeParams.RelatedDataBlock = ConfigPtr;
-
-#if HAS_LEGACY_WRAPPER
-	status = AmdLateRunApTask(&ApExeParams);
-#else
 	status = module_dispatch(AMD_LATE_RUN_AP_TASK, &ApExeParams.StdHeader);
 #endif
 
