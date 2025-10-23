1e654e6bc2e624b123a1d9c574c039efa692b96e
Rane <rane+git@junkyard.systems>
2025-10-23 19:28:44 +1100

    mainboard/kcma-d8: Added AGESA KCMA-D8 mainboard

---
+++ src/mainboard/asus/kcma-d8/OemCustomize.c
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 - 2012 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+//#include <Porting.h>
+#include <AGESA.h>
+
+#include <northbridge/amd/agesa/agesawrapper.h>
+#include <PlatformMemoryConfiguration.h>
+
+/*----------------------------------------------------------------------------------------
+ *                        CUSTOMER OVERIDES MEMORY TABLE
+ *----------------------------------------------------------------------------------------
+ */
+
+CONST PSO_ENTRY ROMDATA DefaultPlatformMemoryConfiguration[]  = {
+	NUMBER_OF_DIMMS_SUPPORTED(ANY_SOCKET, ANY_CHANNEL, 2),
+	PSO_END
+};
+
+const struct OEM_HOOK OemCustomize = {
+};
